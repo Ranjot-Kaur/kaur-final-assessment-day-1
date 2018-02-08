84dec62868eedc58c240d4e9acaa0867fd5a262e
Ranjot Kaur <Ranjot.Kaur@mea.ey.net>
2018-02-08 14:19:31 +0530

    day3 final code

--- app/tomato-exercise-three/mockRest.service.js
+++ app/tomato-exercise-three/mockRest.service.js
@@ -55,7 +55,33 @@
 */
 
 /*************** INSERT ANSWERS BELOW IN THE COMMENTED AREA**************
- 
+
+
+	 function getRaidData() {
+            return mockRestService.getListItems("Risks and Issues", {
+                select: "ID,Title,Function/ID,Description,item%20Owner/Title,
+                expand: "Function,item%20Owner,
+                top: 500,
+                filter: "Issue%20Risk%20Category eq 'HighRisk'"
+            }).then(getRaidInfo);
+
+            function getRaidInfo(response) {
+                return response;
+            }
+		}
+		
+		function getWorkPlanData() {
+            return mockRestService.getListItems("Work Plan", {
+                select: "ID,Task%20Owner,Function/ID,Work%20Plan%20Task%20Type,Work%20Plan%20Task%20Status/Title,Due%20Date,
+                expand: "Function,Work%20Plan%20Task%20Status,
+                top: 2000,
+                orderby:"Due%20Date"
+            }).then(getWorkPlanInfo);
+
+            function getWorkPlanInfo(response) {
+                return response;
+            }
+        }
 
 
 
--- app/tomato-exercise-three/tomato-exercise-three.controller.js
+++ app/tomato-exercise-three/tomato-exercise-three.controller.js
@@ -21,6 +21,11 @@
 
             function activateComplete(results) {
                 vm.data = results[0];
+                console.log(vm.data); 
+                var result2 = calculateTotalTomatoes();
+                var result1 = createFirstFormat(vm.data);
+                console.log(result1);
+                console.log(result2);
             }
         }
 
@@ -31,6 +36,35 @@
         }
 
         //Add your code below.
+
+        function calculateTotalTomatoes(){
+            var secondObj={};
+            _.each(vm.data[0].countries,function(ctry){
+                var obj= _.chain(vm.data[3].orders).filter(function(ord){
+                   return ord.Countries.ID==ctry.ID
+                }).sumBy('Qty').value();
+                secondObj['\"'+ctry.Title+'\"']=obj;                
+            });
+            return secondObj;           
+        }
+
+        
+        function createFirstFormat(data) {
+            var resultData = _(data[3].orders).groupBy('Countries.ID').map(function(item, itemId) {
+                var obj = [];
+                var countryName = _(data[0].countries).filter({ID: parseInt(itemId)}).value()[0].Title;
+                _.forEach(item, function(element) {
+                    element['Countries'] = _(data[0].countries).filter({ID: parseInt(itemId)}).value();
+                    element['Tomato'] = _(data[1].tomatoes).filter({ID: parseInt(element.Tomato.ID)}).value();
+                    element['Status'] = _(data[2].statuses).filter({ID: parseInt(element.Status.ID)}).value();
+                });
+                obj.push(countryName, item);
+                return obj;
+            }).fromPairs().value();
+            return resultData;     
+        }
+
+       
 
 
     }
